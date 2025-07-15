f2f6a067ae864380b62d9cb1002e99b9d72d8715
Esther <mulukiesther@gmail.com>
2025-07-15 11:59:30 -0500

    Initial Commit

---
+++ RingClone/RingCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingCloneApp.swift
+//  RingClone
+//
+//  Created by Esther Nzomo on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RingCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RingCloneTests/RingCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  RingCloneTests.swift
+//  RingCloneTests
+//
+//  Created by Esther Nzomo on 7/15/25.
+//
+
+import Testing
+@testable import RingClone
+
+struct RingCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
